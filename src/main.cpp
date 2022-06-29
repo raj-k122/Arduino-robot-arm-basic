37e30a87356bfaa2c9d0c37b45d5af046ee935e8
Raj Kulkarni <rajkulkarni122@gmail.com>
2022-06-29 18:57:55 +1000

    eh

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <Servo.h>
 #include <Arduino.h>
-Servo servoHIP;
-Servo servoSHOULDER;
-Servo servoELBOW;
-Servo servoWRIST;
+#include <Adafruit_PWMServoDriver.h>
+
+#define stepPinBASE 5
+#define dirPinBASE 6
+
+#define servoHIP 1
+#define servoSHOULDER 2
+#define servoELBOW 3
+#define servoWRIST 4
+
 int pos = 0;
 char input;
+
+/* Create an instance of the library */
+Adafruit_PWMServoDriver Driver(); // Library using B010101 (A5-A0) i2c address, and default Wire @400kHz
+
+// needs link to servo drivers (base motor)
+#define SCL A1
+#define SDA A2
 
 int limiterFunction(int position){
     if(position > 180){
@@ -21,14 +34,16 @@ int limiterFunction(int position){
 
 
 void setup(){
-    servoHIP.attach(3);
-    servoSHOULDER.attach(4);
-    servoELBOW.attach(5);
-    servoWRIST.attach(6);
-    Serial.begin(9600);
+
+    Serial.begin(115200); 
+    Driver.setPWMFreq(50);
+    Driver.begin();
+
+
 }
 
 void loop(){
+
     if (Serial.available()){
         input = Serial.read();
         //H and G is for HIP SERVO
@@ -36,14 +51,14 @@ void loop(){
             pos++;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            servoHIP.write(pos);
+            Driver.setPWM()
 
         }
         if (input == 'g'){
             pos--;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            servoHIP.write(pos);
+            Driver(servoHIP, pos);
 
         }
         //S and A is for SHOULDER SERVO
@@ -51,14 +66,14 @@ void loop(){
             pos++;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            servoSHOULDER.write(pos);
+            Driver(servoSHOULDER, pos);
 
         }
         if (input == 'a'){
             pos--;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            servoSHOULDER.write(pos);
+            Driver(servoSHOULDER, pos);
 
         }
         // //E and R is for ELBOW SERVO
@@ -66,14 +81,14 @@ void loop(){
             pos++;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            servoELBOW.write(pos);
+            Driver(servoELBOW, pos);
 
         }
         if (input == 'r'){
             pos--;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            servoELBOW.write(pos);
+            Driver(servoELBOW, pos);
 
         }
         // //W and Q is for WRIST SERVO
@@ -81,14 +96,14 @@ void loop(){
             pos++;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            servoWRIST.write(pos);
+            Driver(servoWRIST, pos);
 
         }
         if (input == 'q'){
             pos--;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            servoWRIST.write(pos);
+            Driver(servoWRIST, pos);
 
         }
 
