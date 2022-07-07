76496ff57fafe2ee4f075e045d3bbbd3dddb5379
raj-k122 <rajkulkarni122@gmail.com>
2022-07-07 19:34:32 +1000

    initial testing with driver complete

--- Receiver/src/main.cpp
+++ Receiver/src/main.cpp
@@ -1,45 +1,39 @@
-#include <Servo.h>
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
 
-#define stepPinBASE 5
-#define dirPinBASE 6
+#define SERVOMIN 115
+#define SERVOMAX 500
 
-#define servoHIP 1
-#define servoSHOULDER 2
-#define servoELBOW 3
-#define servoWRIST 4
+#define servoHIP 0
+#define servoSHOULDER 1
+#define servoELBOW 2
+#define servoWRIST 3
 
-int pos = 0;
+#define I2CAdd 0x40
+
 char input;
+int pos;
 
 /* Create an instance of the library */
-Adafruit_PWMServoDriver Driver(); // Library using B010101 (A5-A0) i2c address, and default Wire @400kHz
-
-// needs link to servo drivers (base motor)
-#define SCL A1
-#define SDA A2
+Adafruit_PWMServoDriver Driver = Adafruit_PWMServoDriver(0x40); 
 
 int limiterFunction(int position){
-    if(position > 180){
-        position = 180;
+    if(position > 500){
+        position = 500;
     }
-    if(position < 0)
+    if(position < 115)
     {
-        position = 0;
+        position = 115;
     }
     return position;
     
 }
 
-
 void setup(){
-
-    Serial.begin(115200); 
-    Driver.setPWMFreq(50);
+    Driver.wakeup();
     Driver.begin();
-
-
+    Driver.setPWMFreq(50);
+    Serial.begin(9600);
 }
 
 void loop(){
@@ -48,62 +42,62 @@ void loop(){
         input = Serial.read();
         //H and G is for HIP SERVO
         if(input == 'h'){
-            pos++;
+            pos = pos + 5;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            Driver.setPWM()
+            Driver.setPWM(servoHIP, 0, pos);
 
         }
         if (input == 'g'){
-            pos--;
+            pos = pos - 5;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            Driver(servoHIP, pos);
+            Driver.setPWM(servoHIP, 0, pos);
 
         }
         //S and A is for SHOULDER SERVO
         if(input == 's'){
-            pos++;
+            pos = pos + 5;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            Driver(servoSHOULDER, pos);
+            Driver.setPWM(servoSHOULDER, 0, pos);
 
         }
         if (input == 'a'){
-            pos--;
+            pos = pos - 5;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            Driver(servoSHOULDER, pos);
+            Driver.setPWM(servoSHOULDER, 0, pos);
 
         }
-        // //E and R is for ELBOW SERVO
+        // // //E and R is for ELBOW SERVO
         if(input == 'e'){
-            pos++;
+            pos = pos + 5;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            Driver(servoELBOW, pos);
+            Driver.setPWM(servoELBOW, 0, pos);
 
         }
         if (input == 'r'){
-            pos--;
+            pos = pos - 5;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            Driver(servoELBOW, pos);
+            Driver.setPWM(servoELBOW, 0, pos);
 
         }
-        // //W and Q is for WRIST SERVO
+        // // //W and Q is for WRIST SERVO
         if(input == 'w'){
-            pos++;
+            pos = pos + 5;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            Driver(servoWRIST, pos);
+            Driver.setPWM(servoWRIST, 0, pos);
 
         }
         if (input == 'q'){
-            pos--;
+            pos = pos - 5;
             pos = limiterFunction(pos);
             Serial.println(pos);
-            Driver(servoWRIST, pos);
+            Driver.setPWM(servoWRIST, 0, pos);
 
         }
 
